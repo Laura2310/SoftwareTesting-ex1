d41621e596e8c3ceda3e5fe64ddb0a631cebcbfe
Laura <laura.danila23@yahoo.com>
2019-06-23 14:05:20 +0300

    adding before on tests

--- src/test/java/com/company/ArraysUtilsTest.java
+++ src/test/java/com/company/ArraysUtilsTest.java
@@ -1,17 +1,25 @@
 package com.company;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 public class ArraysUtilsTest {
 
+    private ArraysUtils arraysUtils;
+
+    @Before
+    public void setUp(){
+        arraysUtils = new ArraysUtils();
+    }
+
     @Test
     public void binarySearch_elementExist(){
         int[] array = {1,3,5,7,9,10};
         int n = 7;
-        int result = new ArraysUtils().binarySearch(array,n);
+        int result = arraysUtils().binarySearch(array,n);
         Assert.assertEquals(3,result);
 
     }
@@ -29,6 +37,7 @@ public class ArraysUtilsTest {
     public void bubbleSort(){
         int[] array = {1,5,7,3,9,10};
         new ArraysUtils().bubbleSort(array);
+        arraysUtils.bubbleSort(array);
         Assert.assertArrayEquals(new int[]{1,3,5,7,9,10},array);
 
     }
